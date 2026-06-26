7825aa0c8c06f30fc9125ed6b09c3450d0bf749d
ishayashika <yashikakumari33@gmail.com>
2026-06-26 15:05:08 +0530

    Initial backend setup with user registration

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,6 @@
+import express from "express";
+const router=express.Router();
+import { registerUser } from "../controllers/authController.js";
+router.post("/register",registerUser);
+
+export default router;
