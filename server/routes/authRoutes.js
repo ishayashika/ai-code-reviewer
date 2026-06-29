ece48eb8e58b3f22e7e9de15cd5a6b12f68d0f7e
ishayashika <yashikakumari33@gmail.com>
2026-06-29 17:38:36 +0530

    refactor: remove unnecessary comments

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,10 +3,7 @@ import { getProfile, loginUser, registerUser } from "../controllers/authControll
 import { protect } from "../middleware/authMiddleware.js";
 const router=express.Router();//create a router
 
-router.post("/register",registerUser);//creates a POST API 
-// endpoint for user registration. When a client sends a POST 
-// request to /register, Express calls the registerUser 
-// controller function to handle the request
+router.post("/register",registerUser);
 
 router.post("/login",loginUser);
 router.get("/profile",protect,getProfile)
